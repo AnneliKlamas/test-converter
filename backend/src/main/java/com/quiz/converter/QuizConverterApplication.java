6595b475062a461bf84053c3708219bff0757c19
AnneliKlamas <annelik98@gmail.com>
2023-11-11 11:45:51 +0200

    #1 spring initial setup

---
+++ backend/src/main/java/com/quiz/converter/QuizConverterApplication.java
@@ -0,0 +1,13 @@
+package com.quiz.converter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizConverterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizConverterApplication.class, args);
+    }
+
+}
