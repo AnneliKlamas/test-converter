ffcde9369022668959c420e859e68437fe042d8d
AnneliKlamas <44496463+AnneliKlamas@users.noreply.github.com>
2024-03-24 19:25:02 +0200

    #10 cleaned up the code

--- backend/src/main/java/com/quiz/converter/services/MoodleXmlCreatorService.java
+++ backend/src/main/java/com/quiz/converter/services/MoodleXmlCreatorService.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Service;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
-import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.*;
