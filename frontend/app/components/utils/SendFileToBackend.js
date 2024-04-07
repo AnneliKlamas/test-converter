bb152c1fd394352a5dbe55022b562dc026549b3d
Tobias Reiter <89039148+TobiasReiter@users.noreply.github.com>
2024-04-07 14:57:43 +0300

    feat: Install converted file automatically from frontend #17 (#19)

--- frontend/app/components/utils/SendFileToBackend.js
+++ frontend/app/components/utils/SendFileToBackend.js
@@ -11,7 +11,35 @@ export const sendFileToBackend = async (file, outputFormat) => {
       },
     );
 
-    return await response.json();
+    if (!response.ok) {
+      throw new Error(`HTTP error ${response.status}`);
+    }
+
+    const fileName = file[0].name.split(".")[0];
+    const blob = await response.blob();
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement("a");
+    link.href = url;
+
+    const fileExtension =
+      outputFormat === "moodle"
+        ? "xml"
+        : outputFormat === "coursera"
+        ? "docx"
+        : "";
+
+    if (fileExtension) {
+      link.download = `${fileName}.${fileExtension}`;
+    } else {
+      throw new Error("Invalid output format");
+    }
+
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    URL.revokeObjectURL(url);
+
+    return { success: true };
   } catch (error) {
     throw error;
   }
