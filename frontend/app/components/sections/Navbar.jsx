b3523eb7f95d00a194b45c10433aa9319b4cf6ab
TobiasReiter <tobias@reiter.ee>
2024-04-07 15:28:27 +0300

    added documentation page

--- frontend/app/components/sections/Navbar.jsx
+++ frontend/app/components/sections/Navbar.jsx
@@ -1,18 +1,28 @@
 import Image from "next/image";
 import tu from "@/app/components/assets/tu.png";
 import data from "../data/info.json";
+import Link from "next/link";
 
 export default function Navbar() {
   return (
     <nav className="h-20 bg-blue flex justify-between">
-      <Image
-        src={tu}
-        alt="logo"
-        priority
+      <Link
+        href="/"
+        passHref
         className="sm:h-14 h-10 w-auto rounded-sm self-center sm:pl-10"
-      />
+      >
+        <Image
+          src={tu}
+          alt="logo"
+          priority
+          className="sm:h-14 h-10 w-auto rounded-sm self-center sm:pl-10"
+          href="/"
+        />
+      </Link>
       <button className="self-center p-1 sm:px-5 border-2 sm:mr-10 rounded-md text-white">
-        {data.navbar.documentation}
+        <Link href="/documentation" passHref>
+          {data.navbar.documentation}
+        </Link>
       </button>
     </nav>
   );
