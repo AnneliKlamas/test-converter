a3c2204e74871337b63dcd6318182d2bd4d050fb
Tobias Reiter <tobias@reiter.ee>
2024-04-14 20:11:04 +0300

    feat: File Controller changes #41

--- backend/src/main/java/com/quiz/converter/controller/FileController.java
+++ backend/src/main/java/com/quiz/converter/controller/FileController.java
@@ -1,11 +1,11 @@
 package com.quiz.converter.controller;
 
+import com.quiz.converter.models.QuizDetails;
 import com.quiz.converter.services.ConverterService;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.math3.util.Pair;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.http.client.MultipartBodyBuilder;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.MultiValueMap;
@@ -17,6 +17,8 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerException;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping(value = "/file/convert")
@@ -25,39 +27,33 @@ public class FileController {
     @Autowired
     ConverterService service;
 
-    @PostMapping(value = "/moodleXML")
-    public ResponseEntity<MultiValueMap<String, HttpEntity<?>>> convertDocFileToMoodleXML(@RequestBody MultipartFile file) throws IOException, ParserConfigurationException, TransformerException {
-        var convertedFileName = file.getOriginalFilename().replace(".docx", "") + "_moodle.xml";
-        var attachments = service.convertDocToMoodle(file);
+    @PostMapping("/moodleXML")
+    public ResponseEntity<Map<String, Object>> convertDocFileToMoodleXML(@RequestBody MultipartFile file) throws IOException, ParserConfigurationException, TransformerException {
+        Pair<byte[], QuizDetails> conversionResult = service.convertDocToMoodle(file);
+        byte[] fileContent = conversionResult.getFirst();
+        QuizDetails details = conversionResult.getSecond();
 
-        var builder = new MultipartBodyBuilder();
-        builder.part("details", attachments.getSecond())
-                .contentType(MediaType.APPLICATION_JSON);
-        builder.part("file", attachments.getFirst())
-                .contentType(MediaType.APPLICATION_XML)
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + convertedFileName);
-        var body = builder.build();
+        String base64EncodedFile = Base64.encodeBase64String(fileContent);
+        Map<String, Object> responseBody = new HashMap<>();
+        responseBody.put("file", base64EncodedFile);
+        responseBody.put("fileName", file.getOriginalFilename().replace(".docx", "") + "_moodle.xml");
+        responseBody.put("details", details);
 
-        return ResponseEntity.ok()
-                .contentType(MediaType.MULTIPART_FORM_DATA)
-                .body(body);
+        return ResponseEntity.ok(responseBody);
     }
 
-    @PostMapping(value = "/courseraDocx")
-    public ResponseEntity<MultiValueMap<String, HttpEntity<?>>> convertDocFileToCourseraDocx(@RequestBody MultipartFile file) throws IOException {
-        var convertedFileName = file.getOriginalFilename().replace(".docx", "") + "_coursera.docx";
-        var attachments = service.convertDocToCoursera(file);
+    @PostMapping("/courseraDocx")
+    public ResponseEntity<Map<String, Object>> convertDocFileToCourseraDocx(@RequestBody MultipartFile file) throws IOException {
+        Pair<byte[], QuizDetails> conversionResult = service.convertDocToCoursera(file);
+        byte[] fileContent = conversionResult.getFirst();
+        QuizDetails details = conversionResult.getSecond();
 
-        var builder = new MultipartBodyBuilder();
-        builder.part("details", attachments.getSecond())
-                .contentType(MediaType.APPLICATION_JSON);
-        builder.part("file", attachments.getFirst())
-                .contentType(MediaType.valueOf("application/vnd.ms-word"))
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + convertedFileName);
-        var body = builder.build();
+        String base64EncodedFile = Base64.encodeBase64String(fileContent);
+        Map<String, Object> responseBody = new HashMap<>();
+        responseBody.put("file", base64EncodedFile);
+        responseBody.put("fileName", file.getOriginalFilename().replace(".docx", "") + "_coursera.docx");
+        responseBody.put("details", details);
 
-        return ResponseEntity.ok()
-                .contentType(MediaType.MULTIPART_FORM_DATA)
-                .body(body);
+        return ResponseEntity.ok(responseBody);
     }
 }
