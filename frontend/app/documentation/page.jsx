3e3136af8fee7fe8d8a3870d3ecebfdc1b1c7483
TobiasReiter <tobias@reiter.ee>
2024-04-14 15:54:46 +0300

    added documentation page

--- frontend/app/documentation/page.jsx
+++ frontend/app/documentation/page.jsx
@@ -1,46 +1,36 @@
+import Link from "next/link";
+import data from "../components/data/info.json";
+
 export default function Documentation() {
   return (
-    <main className="flex flex-col items-center justify-center pt-10 gap-5">
-      <h1 className="text-4xl font-bold text-blue">Documentation</h1>
-      <p>Here, you can find the rules and guidelines for your document.</p>
-      <div className="flex flex-col items-center justify-center">
-        <h2 className="text-lg font-semibold text-blue">
-          Rules and Guidelines
-        </h2>
-        <ul className="list-disc pl-5 items-center justify-center">
-          <li>
-            Question descriptions nor answers can't start with the words
-            "question", "feedback", nor "\t".
-          </li>
-          <li>Feedback doesn't support pictures.</li>
-          <li>
-            Answers and question descriptions can have pictures, which are
-            always added to the end of the text.
-          </li>
-          <li>Asterisks (*) mark the correct answers.</li>
-          <li>
-            Supports only single choice and multiple choice/checkbox questions.
-          </li>
-          <li>Doesn't support multi-paragraph question descriptions.</li>
-          <li>
-            Coursera doesn't support default feedback for multi nor single
-            choice questions.
-          </li>
-          <li>
-            By default, there is no shuffle in the order of questions or
-            answers.
-          </li>
-          <li>
-            For multiple choice questions in Moodle, every correct answer gives
-            100/nrOfCorrectAnswers percentage points, and every incorrect answer
-            gives -100/nrOfCorrectAnswers percentage points. In Coursera, it
-            behaves as "no partial credit".
-          </li>
-        </ul>
+    <main className="flex flex-col items-center justify-center pt-10">
+      <h1 className="text-4xl font-bold text-blue">
+        {data.documentation.title}
+      </h1>
+      <div className="flex flex-row gap-20 pt-16">
+        <div className="flex gap-3 flex-col items-center justify-center">
+          <h2 className="text-lg font-semibold text-blue">
+            {data.documentation.howTo}
+          </h2>
+          <button className="mt-4 px-7 text-base p-2 text-blue rounded border-2 border-blue hover:bg-blue hover:text-white transition duration-300 ease-in-out">
+            {data.documentation.doc}
+          </button>
+        </div>
+        <div className="flex gap-3 flex-col items-center justify-center">
+          <h2 className="text-lg font-semibold text-blue">
+            {data.documentation.template}
+          </h2>
+          <button className="mt-4 px-7 text-base p-2 text-blue rounded border-2 border-blue hover:bg-blue hover:text-white transition duration-300 ease-in-out">
+            <Link
+              legacyBehavior
+              passHref
+              href="https://docs.google.com/document/d/106xTDS6IG7fofL6Ju4JnqntjxLeBfBM_LIyjKtskFb8/edit"
+            >
+              <a target="_blank"> {data.documentation.templateName}</a>
+            </Link>
+          </button>
+        </div>
       </div>
-      <button className="px-4 py-2 bg-blue-500 text-blue border border-3 rounded-md hover:bg-gray-100">
-        Download .DOCX Template
-      </button>
     </main>
   );
 }
